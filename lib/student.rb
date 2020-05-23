07861e3e74e16d8964671c280eab49f2959f59b0
Jack Madden <jck13mad@gmail.com>
2020-05-23 15:53:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ attr_accessor :id, :name, :grade
   end     
  end
 
- def self.create(name:, grade:)
+ def self.create(name, grade)
     student = self.new(name, grade)
     student.save
     student
