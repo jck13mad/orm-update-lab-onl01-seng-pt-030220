29d369225a71c23e0f17c8d837ec66a143ea1b5f
Jack Madden <jck13mad@gmail.com>
2020-05-23 15:26:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,6 +58,9 @@ class Student
   def self.find_by_name
     sql = "SELECT * FROM students WHERE name = ?"
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first 
+  end
+  
+  def update
   
   
 
